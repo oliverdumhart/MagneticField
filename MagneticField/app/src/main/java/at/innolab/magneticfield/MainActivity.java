69e5e50721fddf2274ecbcfbcff3d1762830f1a9
Oliver Dumhart <oliver@dumhart.net>
2020-04-16 15:34:31 +0200

    Add button and calculate average

--- MagneticField/app/src/main/java/at/innolab/magneticfield/MainActivity.java
+++ MagneticField/app/src/main/java/at/innolab/magneticfield/MainActivity.java
@@ -8,16 +8,22 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private SensorManager sensorManager;
-    private Sensor uncalibratedSensor;
-    private Sensor calibratedSensor;
+    private Sensor sensor;
     private TextView calibratedTextView;
-    private TextView uncalibratedTextView;
+    private Handler handler = new Handler();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        uncalibratedSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
-        calibratedSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         calibratedTextView = findViewById(R.id.calibratedTextView);
-        uncalibratedTextView = findViewById(R.id.uncalibratedTextView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startListening();
+            }
+        });
     }
+
+    private List<Float> xValues = new ArrayList<>();
+    private List<Float> yValues = new ArrayList<>();
+    private List<Float> zValues = new ArrayList<>();
 
     private SensorEventListener calibratedSensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
-            StringBuilder builder = new StringBuilder();
-            for(float v : sensorEvent.values)
-                builder.append(v);
-            
-            calibratedTextView.setText(builder.toString());
+            xValues.add(sensorEvent.values[0]);
+            yValues.add(sensorEvent.values[1]);
+            zValues.add(sensorEvent.values[2]);
         }
 
         @Override
@@ -48,34 +62,47 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private SensorEventListener uncalibratedSensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            StringBuilder builder = new StringBuilder();
-            for(float v : sensorEvent.values)
-                builder.append(v);
+    private void startListening(){
+        button.setEnabled(false);
+        xValues.clear();
+        yValues.clear();
+        zValues.clear();
 
-            uncalibratedTextView.setText(builder.toString());
-        }
+        sensorManager.registerListener(calibratedSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                stopListening();
+                float x = calcAvg(xValues);
+                float y = calcAvg(yValues);
+                float z = calcAvg(zValues);
+                calibratedTextView.setText(x + "\n" + y + "\n" + z);
+            }
+        }, 1_000);
+    }
 
-        }
-    };
+    private void stopListening(){
+        button.setEnabled(true);
+        sensorManager.unregisterListener(calibratedSensorListener);
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
 
-        sensorManager.registerListener(uncalibratedSensorListener, uncalibratedSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(calibratedSensorListener, calibratedSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    private float calcAvg(List<Float> values){
+        float sum = 0;
+        for(float v : values){
+            sum += v;
+        }
+        return sum/values.size();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(uncalibratedSensorListener);
         sensorManager.unregisterListener(calibratedSensorListener);
     }
 }
